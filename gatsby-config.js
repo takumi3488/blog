db2b2a4238b7360ba2e25c24bb15ad21ee208cda
TakumiMori <51111242+tak3488@users.noreply.github.com>
2021-07-29 05:08:07 +0900

    Gatsby Cloud削除

--- gatsby-config.js
+++ gatsby-config.js
@@ -136,10 +136,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
     `gatsby-plugin-postcss`,
     `gatsby-plugin-graphql-codegen`,
     `gatsby-plugin-use-query-params`,
