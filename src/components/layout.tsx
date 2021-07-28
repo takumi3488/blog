39f10a39c7ba49be6d1d2b2c7616406b744a17c8
TakumiMori <51111242+tak3488@users.noreply.github.com>
2021-07-29 00:46:02 +0900

    Footer作成

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,11 +5,11 @@ import { Link } from "gatsby"
 
 const Layout: FC<{ isRoot?: boolean }> = ({ children }) => {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <div className="flex justify-center w-full">
+      <div className="flex justify-center w-full flex-1">
         <main className="flex-1">{children}</main>
-        <aside className="hidden md:block w-40 p-6 min-h-screen">
+        <aside className="hidden md:block w-40 p-6">
           <h2 className="font-semibold mb-4">タグ一覧</h2>
           {Object.keys(tags).map(tag => (
             <Link to={`/?tag=${tag}`}>
@@ -18,12 +18,10 @@ const Layout: FC<{ isRoot?: boolean }> = ({ children }) => {
           ))}
         </aside>
       </div>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      <footer className="w-full text-center text-white bg-gray-800 p-4">
+        <small>© {new Date().getFullYear()} Takumi Mori</small>
       </footer>
-    </>
+    </div>
   )
 }
 
