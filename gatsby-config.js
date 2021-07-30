eb9efea23ccf1e64cb27f9613859494a064caf8b
TakumiMori <51111242+tak3488@users.noreply.github.com>
2021-07-30 21:49:11 +0900

    gatsby-remark-line-breaks削除

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,6 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
-          `gatsby-remark-line-breaks`
         ],
       },
     },
