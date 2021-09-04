6367af63a9a1f51309622bc089a5c51d1349e961
TakumiMori <51111242+tak3488@users.noreply.github.com>
2021-09-04 19:36:39 +0900

    フッター広告削除

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -31,14 +31,14 @@ const Layout: FC<{ title: string; searchTag?: string }> = ({
       <div className="flex justify-center w-full md:w-5/6 lg:w-2/3 xl:w-1/2 xl:max-w-screen-md flex-1 m-auto">
         <main className="flex-1">
           {children}
-          <ins
+          {/* <ins
             className="adsbygoogle"
             style={{ display: "block", margin: "20px auto" }}
             data-ad-client="ca-pub-4022474033409111"
             data-ad-slot="5187044897"
             data-ad-format="auto"
             data-full-width-responsive="true"
-          /> 
+          />  */}
         </main>
         <aside className="hidden md:block w-40 p-6">
           <h2 className="font-semibold mb-4">タグ一覧</h2>
